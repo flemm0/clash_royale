f75517d3c9797d9b05cf3889822b1691bcedc0bc
Flemming Wu <flemming.wu@gmail.com>
2023-11-21 17:26:02 -0800

    add card info to staging table

--- clash_royale_etl_project/assets.py
+++ clash_royale_etl_project/assets.py
@@ -23,6 +23,14 @@ def locations():
     con = duckdb.connect(f'md:clash_royale?motherduck_token={motherduck_token}')
     con.execute('CREATE TABLE IF NOT EXISTS stg_locations AS SELECT * FROM df')
 
+@asset
+def card_info():
+    card_url = 'https://api.clashroyale.com/v1/cards'
+    response = requests.get(url=card_url, headers=headers).json()
+    items = response['items']
+    df = pl.DataFrame(items)
+    con = duckdb.connect(f'md:clash_royale?motherduck_token={motherduck_token}')
+    con.execute('CREATE TABLE IF NOT EXISTS stg_cards AS SELECT * from df')
 
 # configure DuckDB resource
 defs = Definitions(
@@ -31,3 +39,12 @@ defs = Definitions(
         'duckdb': DuckDBResource(database=f'md:?motherduck_token={motherduck_token}')
     }
 )
+
+
+'''
+battle log url:
+https://api.clashroyale.com/v1/players/{player_tag}/battlelog
+
+rankings of players by season
+https://api.clashroyale.com/v1/locations/global/seasons/{season_id e.g. 10}/rankings/players -> player_tag, name, rank
+'''
