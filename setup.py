30e9c87a8ee94c0fd00d954d4ec24cab74d292bd
Flemming Wu <flemming.wu@gmail.com>
2023-11-21 16:20:59 -0800

    add requirements.txt to setup.py file

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
 setup(
     name="clash_royale_etl_project",
     packages=find_packages(exclude=["clash_royale_etl_project_tests"]),
     install_requires=[
+        required,
         "dagster",
         "dagster-cloud"
     ],
