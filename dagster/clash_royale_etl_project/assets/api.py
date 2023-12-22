bd7ecf4fa6a1f3f05dc34d4f7f9c1384de24e423
Flemming Wu <flemming.wu@gmail.com>
2023-12-21 20:05:45 -0800

    moving dagster asset definitions to folder

--- dagster/clash_royale_etl_project/assets/api.py
+++ dagster/clash_royale_etl_project/assets/api.py
@@ -5,7 +5,7 @@ import json
 import duckdb
 import polars as pl
 
-from dagster import asset, Definitions
+from dagster import asset, Definitions, RetryPolicy, OpExecutionContext
 from dagster_duckdb import DuckDBResource
 
 load_dotenv()
@@ -67,8 +67,8 @@ def season_rankings() -> list:
 
     return tbl['player_tag'].to_list()
 
-@asset
-def player_data(season_rankings: list):
+@asset(retry_policy=RetryPolicy(max_retries=3, delay=30))
+def player_data(context: OpExecutionContext, season_rankings: list) -> None:
     '''Takes in player tags from `season_rankings()` and extracts player data from API'''
     con = duckdb.connect(f'md:clash_royale?motherduck_token={motherduck_token}')
     create_table_query = '''
@@ -126,6 +126,7 @@ def player_data(season_rankings: list):
         'totalDonations',
         'warDayWins'
     ]
+    insert_counter = 0
     for player_tag in set(season_rankings).difference(existing_players):
         formatted_tag = player_tag.replace('#', '%23')
         player_url = f'https://api.clashroyale.com/v1/players/{formatted_tag}'
@@ -165,6 +166,9 @@ def player_data(season_rankings: list):
                 VALUES ({placeholders})
             '''
             con.execute(insert_query, list(filtered_data.values()))
+            insert_counter += 1
+            if insert_counter % 100 == 0:
+                context.log.info(f'Inserted {insert_counter} results.')
 
 def unnest_all(df: pl.DataFrame, seperator="_"):
     def _unnest_all(struct_columns):
@@ -188,9 +192,36 @@ def unnest_all(df: pl.DataFrame, seperator="_"):
     return df
 
 @asset
-def battle_data(season_rankings) -> None:
+def battle_data(context: OpExecutionContext, season_rankings: list) -> None:
     '''Data on recent battles that players have participated in'''
-    tbl = None
+    con = duckdb.connect(f'md:clash_royale?motherduck_token={motherduck_token}')
+    cols = [
+        'type', 'battleTime', 'isLadderTournament', 'arena', 'gameMode', 'deckSelection',
+        'team', 'opponent', 'isHostedMatch', 'leagueNumber', 'challengeId', 
+        'challengeWinCountBefore', 'challengeTitle'
+    ]
+    # check if table exists
+    result = con.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='stg_battle_data'").fetchall()
+    if len(result) == 0:
+        create_table_query = '''
+            CREATE TABLE stg_battle_data(
+                type VARCHAR,
+                battleTime VARCHAR,
+                isLadderTournament BOOLEAN,
+                arena STRUCT(id INT, name VARCHAR),
+                gameMode STRUCT(id INT, name VARCHAR),
+                deckSelection VARCHAR,
+                team STRUCT(tag VARCHAR, "name" VARCHAR, crowns BIGINT, kingTowerHitPoints BIGINT, princessTowersHitPoints BIGINT[], clan STRUCT(tag VARCHAR, "name" VARCHAR, badgeId BIGINT), cards STRUCT("name" VARCHAR, id BIGINT, "level" BIGINT, starLevel BIGINT, maxLevel BIGINT, elixirCost BIGINT, iconUrls STRUCT(medium VARCHAR, evolutionMedium VARCHAR), evolutionLevel BIGINT, maxEvolutionLevel BIGINT)[], globalRank JSON, elixirLeaked DOUBLE, trophyChange BIGINT, startingTrophies BIGINT)[],
+                opponent STRUCT(tag VARCHAR, "name" VARCHAR, crowns BIGINT, kingTowerHitPoints BIGINT, princessTowersHitPoints BIGINT[], clan STRUCT(tag VARCHAR, "name" VARCHAR, badgeId BIGINT), cards STRUCT("name" VARCHAR, id BIGINT, "level" BIGINT, starLevel BIGINT, maxLevel BIGINT, elixirCost BIGINT, iconUrls STRUCT(medium VARCHAR, evolutionMedium VARCHAR), evolutionLevel BIGINT, maxEvolutionLevel BIGINT)[], globalRank JSON, elixirLeaked DOUBLE, trophyChange BIGINT, startingTrophies BIGINT)[],
+                isHostedMatch BOOLEAN,
+                leagueNumber INTEGER,
+                challengeId INTEGER,
+                challengeWinCountBefore INTEGER,
+                challengeTitle VARCHAR
+            )
+        '''
+        con.execute(create_table_query)
+    insert_counter = 0
     for player_tag in season_rankings:
         formatted_tag = player_tag.replace('#', '%23')
         player_url = f'https://api.clashroyale.com/v1/players/{formatted_tag}/battlelog'
@@ -198,14 +229,15 @@ def battle_data(season_rankings) -> None:
         if response.status_code == 200:
             data = response.json()
             if data:
-                df = pl.DataFrame(data)
-                df = unnest_all(unnest_all(df.explode(['team', 'opponent'])).explode('team_cards').explode('opponent_cards'))
-                if tbl is None:
-                    tbl = df
-                else:
-                    tbl = pl.concat([tbl, df], how='diagonal_relaxed')
-    con = duckdb.connect(f'md:clash_royale?motherduck_token={motherduck_token}')
-    con.execute('CREATE OR REPLACE TABLE stg_battle_data AS SELECT * FROM tbl')
+                standardized_data = {col: data[col] if col in data else None for col in cols}
+                df = pl.DataFrame(standardized_data)
+                ## keep nested JSON as struct, and unnest in transformation stage
+                #df = unnest_all(unnest_all(df.explode(['team', 'opponent'])).explode('team_cards').explode('opponent_cards'))
+                con.execute('INSERT INTO stg_battle_data SELECT * FROM df')
+                insert_counter += 1
+                if insert_counter % 100 == 0:
+                    context.log.info(f'Inserted battle data for {insert_counter} players.')
+
 
 # configure DuckDB resource
 defs = Definitions(
@@ -221,4 +253,6 @@ battle log url:
 https://api.clashroyale.com/v1/players/{player_tag}/battlelog
 
 hitpoints = how much HP is left on tower at end of game
+
+possible missing from battle data: team_trophyChange, team_cardsEvolutionLevel
 '''
