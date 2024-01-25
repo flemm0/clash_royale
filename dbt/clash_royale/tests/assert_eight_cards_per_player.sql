5b9049a6a5eec359527e7bfab1feeff7963908cf
Flemming <flemming.wu@gmail.com>
2024-01-24 16:42:04 -0800

    remove old tests

--- dbt/clash_royale/tests/assert_eight_cards_per_player.sql
+++
@@ -1,8 +0,0 @@
--- every player must play with a deck of eight unique cards
-select
-    battle_id,
-    player_tag,
-    count(card_id) as card_count
-from {{ ref('battle_cards_bridge') }}
-group by 1, 2
-having card_count <> 8
