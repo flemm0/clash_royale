fb0b33485e3f3537e3ab7c7ac7f0f8bf1dc21921
Ubuntu <flemming@second-instance.fan4vysnne0uboszujydnzxafb.phxx.internal.cloudapp.net>
2024-01-28 23:44:18 +0000

    adding skeleton code for daily fact table update job

--- dagster/clash_royale_etl_project/__init__.py
+++ dagster/clash_royale_etl_project/__init__.py
@@ -5,18 +5,27 @@ from .assets import players, cards, clans, seasons, dbt_assets, constants
 
 from .resources import database_resource
 
+from .schedules import fact_table_update_schedule
+
+from .jobs import daily_fact_table_update
+
 from dagster_dbt import DbtCliResource
 
-import os
 
 
 python_assets = load_assets_from_modules([cards, clans, seasons, players])
 dbt_assets = load_assets_from_modules([dbt_assets])
+
+all_jobs = [daily_fact_table_update]
+
+all_schedules = [daily_fact_table_update]
 
 defs = Definitions(
     assets=[*python_assets, *dbt_assets],
     resources={
         'database': database_resource,
         'dbt': DbtCliResource(project_dir=os.fspath(constants.dbt_project_dir))
-    }
+    },
+    jobs=all_jobs,
+    schedules=all_schedules,
 )
